064d54e9ba6a8a4aeb66ee292cb402f066c2a688
DanielLodder <94366248+DanielLodder@users.noreply.github.com>
2024-11-25 11:36:00 +0100

    BonePulling: updated logic & bone behavior

--- 2024Shortburn/Assets/Scripts/Bone.cs
+++ 2024Shortburn/Assets/Scripts/Bone.cs
@@ -2,12 +2,20 @@ using UnityEngine;
 
 public class Bone : MonoBehaviour
 {
+    private bool isMoving;
     public float health;
     private Vector3 respawnPoint;
 
     void Update()
     {
         HealthCheck(health);
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bone" && isMoving == true)
+        {
+            health--;
+        }
     }
     private void HealthCheck(float health)
     {
--- 2024Shortburn/Assets/Scripts/BonePulling.cs
+++ 2024Shortburn/Assets/Scripts/BonePulling.cs
@@ -3,10 +3,16 @@ using UnityEngine.InputSystem;
 
 public class BonePulling : MonoBehaviour
 {
-    Bone selectedBone;
+    GameManager gameManager;
+    GameObject selectedBone;
     PlayerInput recieveInput;
     private Vector2 moveDirection;
     //bool isPulling(PlayerState);
+    private void Start()
+    {
+        gameManager = GameManager.instance;
+        HoldingBehavior.PickingUp += SelectBone;
+    }
     private void HandleOnPull(InputAction.CallbackContext ctx)
     {
         moveDirection = ctx.ReadValue<Vector2>();
@@ -18,8 +24,19 @@ public class BonePulling : MonoBehaviour
         recieveInput.actions["Move"].started += HandleOnPull;
         recieveInput.actions["Move"].canceled += HandleOnPull;
     }
+    private void SelectBone(GameObject obj)
+    {
+        if (obj.CompareTag("Bone"))
+        {
+            selectedBone = obj;
+        }
+    }
     private void Update()
     {
-        selectedBone.transform.Translate(moveDirection.x * Time.deltaTime * 5f, 0f, moveDirection.y * Time.deltaTime * 5f);
+
+        if (selectedBone != null)
+        {
+            selectedBone.transform.Translate(moveDirection.x * Time.deltaTime * 5f, 0f, moveDirection.y * Time.deltaTime * 5f);
+        }
     }
 }
